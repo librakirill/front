a4ad1d1a09539aebbfc3b33b102dab79ca06799c
kirik <librakirill69@gmail.com>
2019-06-23 22:15:55 +0300

    задание к первому уроку

---
+++ les_1.py
@@ -0,0 +1,36 @@
+A:=1; # не нужна точка с запятой и ошибка с двоеточием
+B-A = C # b переменная не определена
+A + B = C # двоеточие не нужно и переменная b не определена
+
+
+a = 10
+b = 'сторка'
+#с = True
+
+Num = 1
+Text = 'Text'
+Exp = Num + Text
+print (Exp)
+
+#не возможно сложить два типа данных str и int
+
+A = 5
+A += 5
+print (A)
+
+#прибавит к переменной "A" 5
+
+
+first = int(input("введите первое число"))
+second = int(input("введите второе число"))
+result = first + second
+print (result)
+
+a = 1
+b = 2
+a = b
+print(a)
+print(b)
+b = a
+print(a)
+print(b)
